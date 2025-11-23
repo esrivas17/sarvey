fixing calculation of amplitudes and offsets

sarvey/densification.py
@@ -41,6 +41,7 @@ from sarvey.unwrapping import oneDimSearchTemporalCoherence
 from sarvey.unwrapping_seasonal import oneDimSearchTemporalCoherence2
 from sarvey.objects import Points
 import sarvey.utils as ut
+from tqdm import tqdm
 import pdb
 
 def densificationInitializer(tree_p1: KDTree, point2_obj: Points, demod_phase1: np.ndarray):
@@ -196,13 +197,6 @@ def launchDensifyStarNetworkConsistencyCheck(args: tuple):
 
         design_mat[:, 2] = factor * np.cos(omega * global_point2_obj.ifg_net_obj.tbase_ifg)
         design_mat[:, 3] = factor * np.sin(omega * global_point2_obj.ifg_net_obj.tbase_ifg)
-
-        #demerr_point2, vel_point2, gamma_point2 = oneDimSearchTemporalCoherence(
-        #    demerr_range=demerr_range,
-        #    vel_range=vel_range,
-        #    obs_phase=arc_phase_p1,
-        #    design_mat=design_mat
-        #)
 
         demerr_p2[idx], vel_p2[idx], amplitude_p2[idx], offset_p2[idx], gamma_p2[idx] = oneDimSearchTemporalCoherence2(demerr_range=demerr_range, 
                                                                                               vel_range=vel_range, 

sarvey/unwrapping_seasonal.py
@@ -101,8 +101,8 @@ def findOptimum2D(*, obs_phase: np.ndarray, design_mat: np.ndarray, amps_range: 
     amps_vals, offset_vals = np.meshgrid(amps_range, offset_range, indexing='ij') 
 
     # pred phase
-    sine_part = amps_vals.ravel() * np.sin(omega*offset_vals.ravel())
     cosine_part = amps_vals.ravel() * np.cos(omega*offset_vals.ravel())
+    sine_part = amps_vals.ravel() * np.sin(omega*offset_vals.ravel())
     
     pred_phase = design_mat[:,0] * cosine_part[:,np.newaxis] + design_mat[:,1] * sine_part[:,np.newaxis]
 
@@ -255,19 +255,68 @@ def oneDimSearchTemporalCoherence2(*, demerr_range: np.ndarray, vel_range: np.nd
     scale_vel = vel_range.max()
     scale_amp = amp_range.max()
     scale_offset = offset_range.max()
-    initial_guess = np.array([demerr/scale_demerr, vel/scale_vel, amp/scale_amp, offset/scale_offset]).T
+    cosine_part = amp*np.cos(omega*offset)
+    sine_part = amp*np.sin(omega*offset)
+    scale_cos = np.max(space_cos)
+    scale_sin = np.max(space_sin)
+    initial_guess = np.array([demerr/scale_demerr, vel/scale_vel, cosine_part/scale_cos, sine_part/scale_sin]).T
+    #initial_guess = np.array([demerr/scale_demerr, vel/scale_vel, amp/scale_amp, offset/scale_offset]).T
 
-    demerr, vel, amp, offset, gamma = gradientSearchTemporalCoherence2(scale_vel=scale_vel, scale_demerr=scale_demerr, scale_amp=scale_amp, scale_offset=scale_offset, 
+    #demerr, vel, amp, offset, gamma = gradientSearchTemporalCoherence2(scale_vel=scale_vel, scale_demerr=scale_demerr, scale_amp=scale_amp, scale_offset=scale_offset, 
+    #                                                      obs_phase=obs_phase, design_mat=design_mat, omega=omega, x0=initial_guess)
+    
+    demerr, vel, cosine, sine, gamma = gradientSearchTemporalCoherenceLinear(scale_vel=scale_vel, scale_demerr=scale_demerr, scale_cosine=scale_cos, scale_sine=scale_sin, 
                                                           obs_phase=obs_phase, design_mat=design_mat, omega=omega, x0=initial_guess)
-    cos_term = amp * np.cos(omega*offset)
-    sin_term = amp * np.sin(omega*offset)
 
-    pred_phase = np.matmul(design_mat, np.array([demerr, vel, cos_term, sin_term]))
+    #cos_term = amp * np.cos(omega*offset)
+    #sin_term = amp * np.sin(omega*offset)
+
+    pred_phase = np.matmul(design_mat, np.array([demerr, vel, cosine, sine]))
+
+    amp = np.sqrt(cosine**2+sine**2)
+    offset = np.arctan2(sine, cosine)/omega
+
     res = (obs_phase - pred_phase.T).ravel()
     gamma = np.abs(np.mean(np.exp(1j * res)))
 
     return demerr, vel, amp, offset, gamma
 
+
+def gradientSearchTemporalCoherenceLinear(*, scale_vel: float, scale_demerr: float, scale_cosine: float, scale_sine, obs_phase: np.ndarray,
+                                    design_mat: np.ndarray, omega: float, x0: np.ndarray):
+    bounds = ((-1, 1), (-1, 1), (0, 1), (0,1))
+    opt_res = minimize(objFuncTempCohLinear, x0, args=(design_mat, obs_phase, scale_vel, scale_demerr, scale_cosine, scale_sine, omega),  bounds=bounds, method='L-BFGS-B')
+    #With Jacobian
+    #opt_res = minimize(objFuncTempCoh2, x0, args=(design_mat, obs_phase, scale_vel, scale_demerr, scale_amp, scale_offset, omega),  
+    #                   bounds=bounds, method='L-BFGS-B', jac=Jacobian)
+    gamma = 1 - opt_res.fun
+    demerr = opt_res.x[0] * scale_demerr
+    vel = opt_res.x[1] * scale_vel
+    cosine_part = opt_res.x[2] * scale_cosine
+    sine_part = opt_res.x[3]*scale_sine
+
+    #amp = np.sqrt(cosine_part**2+sine_part**2)
+    #offset = np.arctan2(sine_part, cosine_part)/omega
+
+    return demerr, vel, cosine_part, sine_part, gamma
+
+
+def objFuncTempCohLinear(x, *args):
+    (design_mat, obs_phase, scale_vel, scale_demerr, scale_cosine, scale_sine, omega) = args
+    
+    #scale_cos = scale_amp * np.sin(omega*scale_offset)
+    #scale_sin = scale_amp * np.cos(omega*scale_offset)
+
+    x[0] *= scale_demerr
+    x[1] *= scale_vel
+    x[2] *= scale_cosine
+    x[3] *= scale_sine
+
+    # pred phase
+    pred_phase = np.matmul(design_mat, x)
+    res = (obs_phase - pred_phase.T).ravel()
+    gamma = np.abs(np.mean(np.exp(1j*res)))
+    return 1-gamma
 
 
 def gradientSearchTemporalCoherence2(*, scale_vel: float, scale_demerr: float, scale_amp: float, scale_offset, obs_phase: np.ndarray,
@@ -276,7 +325,7 @@ def gradientSearchTemporalCoherence2(*, scale_vel: float, scale_demerr: float, s
     opt_res = minimize(objFuncTempCoh2, x0, args=(design_mat, obs_phase, scale_vel, scale_demerr, scale_amp, scale_offset, omega),  bounds=bounds, method='L-BFGS-B')
     #With Jacobian
     #opt_res = minimize(objFuncTempCoh2, x0, args=(design_mat, obs_phase, scale_vel, scale_demerr, scale_amp, scale_offset, omega),  
-    #                    bounds=bounds, method='L-BFGS-B', jac=Jacobian)
+    #                   bounds=bounds, method='L-BFGS-B', jac=Jacobian)
     gamma = 1 - opt_res.fun
     demerr = opt_res.x[0] * scale_demerr
     vel = opt_res.x[1] * scale_vel
@@ -295,7 +344,8 @@ def objFuncTempCoh2(x, *args):
     x[2] *= scale_amp
     x[3] *= scale_offset
 
-    # pred phase
+    # pred phase 
+    # is this non linear? I have no clue what I am doing!!
     params = np.array([x[0], x[1], x[2]*np.cos(omega*x[3]), x[2]*np.sin(omega*x[3])])
     pred_phase = np.matmul(design_mat, params)
     res = (obs_phase - pred_phase.T).ravel()
@@ -339,11 +389,18 @@ def Jacobian(x, *args):
     pred_phase = np.matmul(design_mat, params)
 
     
-    if len(obs_phase.shape) == 2:
-        res = (obs_phase - pred_phase)
+    if len(obs_phase.shape) == 20:
+        # step densification
+        A, P = obs_phase.shape 
+        N = pred_phase.shape[0]
+        res = obs_phase[:, None, :] - pred_phase[None, :, :]
+        res = res.transpose(1, 0, 2)
+        res = res.reshape(N, A * P)
+        e = np.exp(1j * res)
+        #res = (obs_phase - pred_phase)
         g = np.mean(np.exp(1j*res), axis=0)
         gamma = np.abs(np.mean(np.exp(1j * res), axis=0))
-        e = np.exp(1j * res)
+        #e = np.exp(1j * res)
     else:
         res = (obs_phase - pred_phase)
         g = np.mean(np.exp(1j*res))
@@ -409,6 +466,9 @@ def Jacobian2(x, *args):
     pred_phase = design_mat @ params      # shape (N,)
     r = (obs_phase - pred_phase).ravel()  # shape (N,)
 
+
+
+    #r = res
     # --- complex mean and gamma ---
     e = np.exp(1j * r)                    # shape (N,)
     g = e.mean()                          # complex scalar

sarvey/utils.py
@@ -590,6 +590,10 @@ def estimateParametersStar(*, obj: Union[Points, Network], estimate_ref_atmo: bo
     factor = 4 * np.pi / obj.wavelength
     f = 1 # frequency cycles per years
     omg = 2.0 * np.pi * f
+    
+    #a[:, 1] = 4 * np.pi / obj.wavelength * tbase
+    #a[:, 0] = 4 * np.pi / obj.wavelength * pbase / (obj.slant_range[p] * np.sin(obj.loc_inc[p]))  # demerr
+
     a[:, 1] = factor * tbase  # velocity
     a[:, 2] = factor * np.cos(omg * tbase) 
     a[:, 3] = factor * np.sin(omg * tbase)
@@ -601,26 +605,27 @@ def estimateParametersStar(*, obj: Union[Points, Network], estimate_ref_atmo: bo
         x_hat, omega[p] = np.linalg.lstsq(a, obv_vec, rcond=None)[0:2]
         demerr[p] = x_hat[0]
         vel[p] = x_hat[1]
-        amplitude[p] = x_hat[2]
-        offset[p] = x_hat[3]
+        amp = np.sqrt(x_hat[2]**2 + x_hat[3]**2)
+        off = np.arctan2(x_hat[3],x_hat[2])/omg
+        amplitude[p] = amp
+        offset[p] = off
+        pred_phase_demerr = a[:,0] * x_hat[0]
+        pred_phase_vel = a[:,1] * x_hat[1]
+        sine_part = x_hat[3]
+        cosine_part = x_hat[2]
+        pred_phase_seasonal = a[:,2] * cosine_part + a[:,3] * sine_part
+
         if estimate_ref_atmo:
             ref_atmo[p] = x_hat[4]
-        v_hat[p, :] = obv_vec - np.matmul(a, x_hat)
-        #v_hat[p, :] = np.angle(np.exp(1j * (obv_vec - np.matmul(a, x_hat))))
+            atmosphere = a[:,4] * x_hat[4]
+            pred_phase = pred_phase_demerr + pred_phase_vel + pred_phase_seasonal + atmosphere
+
+        else:
+            pred_phase = pred_phase_demerr + pred_phase_vel + pred_phase_seasonal
+
+        v_hat[p, :] = np.angle(np.exp(1j *(obv_vec - pred_phase)))
+        #v_hat[p, :] = obv_vec - pred_phase
         coherence[p] = np.abs(np.mean(np.exp(1j * v_hat[p, :])))
-        
-        #ph1 = obv_vec
-        #ph2 = np.matmul(a, x_hat)
-        #pha = np.angle(np.exp(1j * (ph1 - ph2)))
-        #res = obv_vec - np.matmul(a, x_hat)
-        #sumex = np.sum(res > np.pi)
-        #sumex += np.sum(res < -np.pi)
-        #print(sumex)
-        #sumex = np.sum(pha > np.pi)
-        #sumex += np.sum(pha < -np.pi)
-        #print(sumex)
-        #pdb.set_trace()
-        #import matplotlib.pyplot as plt
 
     if not estimate_ref_atmo:
         ref_atmo = None