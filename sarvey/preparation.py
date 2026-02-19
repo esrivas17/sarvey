fb96efb2147d53e36f68206f8ee8b2c11f54233a
esrivas17 <esrivas17@gmail.com>
2026-02-19 13:32:06 +0100

    error fxed

--- sarvey/preparation.py
+++ sarvey/preparation.py
@@ -213,7 +213,7 @@ def selectPixels(*, path: str, selection_method: str, thrsh: float,
         # cmap = "lajolla"
 
     elif selection_method == "adi":
-        adi_obj = BaseStack(file=join(path, "temporal_coherence.h5"), logger=logger)
+        adi_obj = BaseStack(file=join(path, "amplitude_dispersion.h5"), logger=logger)
         quality = adi_obj.read(dataset_name="adi")
         logger.debug(f"[Min, Max] of all amplitude dispersion pixels: [{np.min(quality):.2f}, {np.max(quality):.2f}].)")
         logger.debug(f"[Min, Max] of all amplitude dispersion pixels excluding invalid values: "
