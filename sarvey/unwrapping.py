6601a1e77963d087e81bd44b42111b2ad7b9bf3d
esrivas17 <esrivas17@gmail.com>
2026-03-18 12:31:09 +0100

    bugs

--- sarvey/unwrapping.py
+++ sarvey/unwrapping.py
@@ -1119,7 +1119,7 @@ def removeArcsFromNonExistingPoints(*,
 
     # arcs not in points id
     bad_arcs = [(arc[0], arc[1]) for arc in net_obj.arcs if arc[0] not in point_id or arc[1] not in point_id]
-    bad_arc_indices = [ix for ix, arc in enumerate(net_obj.arcs) if arc[0] not in point_id or arc[1] not in idxs_points]
+    bad_arc_indices = [ix for ix, arc in enumerate(net_obj.arcs) if arc[0] not in idxs_points or arc[1] not in idxs_points]
 
     logger.info(msg=f"Number of arcs: {net_obj.num_arcs}. Removing {len(bad_arc_indices)} disconnected arc(s)")
 
