8fbe0b3b43e1b3536b60a5532105b76469c66d2b
esrivas17 <esrivas17@gmail.com>
2026-03-20 17:32:59 +0100

    changes in config

--- sarvey/config.py
+++ sarvey/config.py
@@ -145,6 +145,15 @@ class General(BaseModel, extra="forbid"):
             raise ValueError("Unwrapping method must be either 'ilp' or 'puma'.")
         return v
 
+    @field_validator('adi_path')
+    def checkADIPath(cls, v):
+        """Check if the amplitude dispersion exists."""
+        if v:
+            if not os.path.exists(os.path.abspath(v)):
+                raise ValueError(f"Path is invalid: {os.path.abspath(v)}")
+            else:
+                return v
+
     @field_validator('logging_level')
     def checkLoggingLevel(cls, v):
         """Check if the logging level is valid."""
@@ -359,6 +368,11 @@ class ConsistencyCheck(BaseModel, extra="forbid"):
         default=0.9
     )
 
+    adi_p1: float = Field(
+        title="Maximum ADI threshold for first-order points",
+        description="Set the ADI threshold of first-order points for the consistency check.",
+        default=0.25)
+    
     grid_size: int = Field(
         title="Grid size [m]",
         description="Set the grid size in [m] for the consistency check. No grid is applied if 'grid_size' is Zero.",
@@ -431,10 +445,10 @@ class ConsistencyCheck(BaseModel, extra="forbid"):
     #    description="Set the arc unwrapping coherence threshold for the DEM error network.",
     #    default=0.4)
 
-    min_num_arc_demerror: int = Field(
-        title="Minimum number of arcs per point fro DEM error estimation",
-        description="Set the minimum number of arcs per point.",
-        default=2)
+    #min_num_arc_demerror: int = Field(
+    #    title="Minimum number of arcs per point fro DEM error estimation",
+    #    description="Set the minimum number of arcs per point.",
+    #    default=2)
 
     #dem_error_bound_demerror_network: float = Field(
     #    title="Bounds on DEM error for temporal unwrapping [m] of DEM error network",
@@ -452,8 +466,7 @@ class ConsistencyCheck(BaseModel, extra="forbid"):
     max_arc_height: int = Field(
         title="Height threshhold for network creation",
         description="Set height threshhold for network",
-        default=50
-    )
+        default=50)
     
     
     @field_validator('coherence_p1')
@@ -550,6 +563,12 @@ class Filtering(BaseModel, extra="forbid"):
         title="Temporal coherence threshold",
         description="Set the temporal coherence threshold for the filtering step.",
         default=0.8
+    )
+
+    adi_p2: float = Field(
+        title="Maximum ADI threshold for second-order points",
+        description="Set the ADI threshold of second-order points for the filtering step.",
+        default=0.5
     )
 
     apply_aps_filtering: bool = Field(
@@ -678,11 +697,10 @@ class Densification(BaseModel, extra="forbid"):
         default=0.5
     )
 
-    gamma_thresh_demerror: float = Field(
-        title="Gamma threshold for arc coherence to obtain coarse dem error",
-        description="Gamma threshold for coarse DEM estimation",
-        default=0.5
-    )
+    #gamma_thresh_demerror: float = Field(
+    #    title="Gamma threshold for arc coherence to obtain coarse dem error",
+    #    description="Gamma threshold for coarse DEM estimation",
+    #    default=0.5)
 
     max_height_to_p1: int = Field(
         title="Maximum height to first-order points [m]",
