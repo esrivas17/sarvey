d6b35d00fd7bf0aa4a30944260efde4b8addbc1c
esrivas17 <esrivas17@gmail.com>
2026-07-29 18:34:53 +0200

    more

--- sarvey/processing.py
+++ sarvey/processing.py
@@ -443,15 +443,15 @@ class Processing:
         # histogram
         fig = plt.figure(figsize=(16, 5))
         axs = fig.subplots(1, 3)
-        axs[0].hist(tcoef*1000, bins=5000)
+        axs[0].hist(tcoef*1000, bins="auto")
         axs[0].set_ylabel('Absolute frequency')
         axs[0].set_xlabel('Temp Coeff [mm/C]')
 
-        axs[1].hist(demerr, bins=5000)
+        axs[1].hist(demerr, bins="auto")
         axs[1].set_ylabel('Absolute frequency')
         axs[1].set_xlabel('DEM error [m]')
 
-        axs[2].hist(vel*100, bins=5000)
+        axs[2].hist(vel*100, bins="auto")
         axs[2].set_ylabel('Absolute frequency')
         axs[2].set_xlabel('Velocity [cm/yer]')
 
